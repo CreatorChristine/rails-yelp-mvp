743afea9c6a8e2dfa0ee8da0905bf889e039d282
Christine Rhee <rheec2016@gmail.com>
2020-05-27 17:03:58 +0900

    all rakes passed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing database..."
+Review.destroy_all
+Restaurant.destroy_all
+puts "Database cleared. Creating 5 restaurants and their reviews..."
+
+5.times do
+    restaurant = Restaurant.create!(
+        name: Faker::Restaurant.name, 
+        address: Faker::Address.full_address, 
+        category: Restaurant::CATEGORIES.sample, 
+        phone_number: Faker::PhoneNumber.phone_number_with_country_code)
+        puts "#{restaurant.name} made."
+    restaurant_id = restaurant.id
+    rand(1..10).times do
+        review = Review.create!(content: Faker::ChuckNorris.fact , rating: Review::RATINGS.sample, restaurant_id: restaurant_id)
+        puts "#{review.content}"
+    end
+end
